a62c478847220641653df33eb4ca2f353d16ca06
Kondaspoorthy <kondasaispoorthy@msitprogram.net>
2021-09-24 19:34:08 +0530

    updated1

--- 2DListex1.py
+++ 2DListex1.py
@@ -8,4 +8,4 @@ print(getTotalpopulation([ ["Pittsburgh", "Allegheny", 302407],
            ["Philadelphia", "Philadelphia", 1584981],
            ["Allentown", "Lehigh", 123838],
            ["Erie", "Erie", 97639],
-           ["Scranton", "Lackawanna", 77182] ]))#function call
+           ["Scranton", "Lackawanna", 77182] ]))#function call 1
